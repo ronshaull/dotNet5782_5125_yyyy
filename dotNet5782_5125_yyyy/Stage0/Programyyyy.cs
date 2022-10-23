33607c3f85b2dd5e463ac9e73b3b754ba7332c5d
ronshaull <ronshaull51@gmail.com>
2022-10-23 17:47:59 +0300

    initial code+welcome methods

---
+++ dotNet5782_5125_yyyy/Stage0/Programyyyy.cs
@@ -0,0 +1,19 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            welcome5125();
+            Console.ReadKey();
+        }
+        static partial void welcomeyyyy();
+        private static void welcome5125()
+        {
+            string name = "";
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first application!", name);
+        }
+    }
+}
